1eae79c3e32b2f356e3a15d09699273fe80d6363
Scott Chase Waggener <tidalpaladin@protonmail.com>
2024-02-24 12:54:19 -0600

    Convert to torch.autograd.Function, tweak benchmark

--- mit_ub/model/kernels/distance.py
+++ mit_ub/model/kernels/distance.py
@@ -1,17 +1,22 @@
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 from functools import partial
 from pathlib import Path
-from typing import Any, cast
+from typing import Any, Callable, cast
 
 import torch
 import triton
 import triton.language as tl
+from torch import Tensor
+from torch.autograd import Function
 from tqdm import tqdm
 
 
 # We expect K to be relatively small (probably length 1-3) for spacial coordinates
 @triton.autotune(
     configs=[
+        triton.Config(
+            {"BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 128, "BLOCK_SIZE_K": 1, "GROUP_SIZE_M": 8}, num_stages=3, num_warps=8
+        ),
         triton.Config(
             {"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 128, "BLOCK_SIZE_K": 2, "GROUP_SIZE_M": 8}, num_stages=3, num_warps=8
         ),
@@ -40,7 +45,7 @@ from tqdm import tqdm
     key=["M", "N", "K"],
 )
 @triton.jit
-def euclidean_distance_kernel(
+def _euclidean_distance_kernel_fwd(
     # Pointers to matrices
     a_ptr: tl.tensor,
     b_ptr: tl.tensor,
@@ -126,33 +131,128 @@ def euclidean_distance_kernel(
     tl.store(c_block_ptr, c, boundary_check=(0, 1))
 
 
-def euclidean_distance(a, b):
-    assert a.shape[-1] == b.shape[-1], "Incompatible dimensions"
-    assert a.is_contiguous(), "Matrix A must be contiguous"
-    assert b.is_contiguous(), "Matrix B must be contiguous"
+@triton.jit
+def _euclidean_distance_kernel_bwd(
+    # Pointers to matrices
+    a_ptr: tl.tensor,
+    b_ptr: tl.tensor,
+    c_ptr: tl.tensor,
+    # Matrix dimensions
+    M: int,
+    N: int,
+    K: int,
+    stride_am: int,
+    stride_ak: int,
+    stride_bn: int,
+    stride_bk: int,
+    stride_cm: int,
+    stride_cn: int,
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_K: tl.constexpr,
+    GROUP_SIZE_M: tl.constexpr,
+):
+    # Map program ids `pid` to the block of C it should compute.
+    # This is done in a grouped ordering to promote L2 data reuse.
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_SIZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    pid_m = first_pid_m + (pid % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
 
-    M, K = a.shape
-    N, K = b.shape
-    c = torch.empty((M, N), device=a.device, dtype=a.dtype)
-
-    def grid(META):
-        return (triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(N, META["BLOCK_SIZE_N"]),)
-
-    cast(Any, euclidean_distance_kernel)[grid](
-        a,
-        b,
-        c,
-        M,
-        N,
-        K,
-        a.stride(0),
-        a.stride(1),
-        b.stride(0),
-        b.stride(1),
-        c.stride(0),
-        c.stride(1),
+    # Create block pointers for A and B
+    # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
+    # `b_ptrs` is a block of [BLOCK_SIZE_N, BLOCK_SIZE_K] pointers
+    a_block_ptr = tl.make_block_ptr(
+        base=a_ptr,
+        shape=(M, K),
+        strides=(stride_am, stride_ak),
+        offsets=(pid_m * BLOCK_SIZE_M, 0),
+        block_shape=(BLOCK_SIZE_M, BLOCK_SIZE_K),
+        order=(1, 0),
     )
-    return c
+    b_block_ptr = tl.make_block_ptr(
+        base=b_ptr,
+        shape=(N, K),
+        strides=(stride_bn, stride_bk),
+        offsets=(pid_n * BLOCK_SIZE_N, 0),
+        block_shape=(BLOCK_SIZE_N, BLOCK_SIZE_K),
+        order=(1, 0),
+    )
+
+    # Outer loop over the K dimension.
+    # We will accumulate the sum of squared differences across K blocks and then take the square root.
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for _ in range(0, K, BLOCK_SIZE_K):
+        # Load blocks of A and B into shared memory for this chunk of K
+        a = tl.load(a_block_ptr, boundary_check=(0, 1))
+        b = tl.load(b_block_ptr, boundary_check=(0, 1))
+
+        # Compute the sum of the squared differences
+        diff = a[:, None] - b[None, :]
+        diff = diff * diff
+        diff = tl.sum(diff, axis=2)
+
+        # Accumulate the result
+        accumulator += diff
+
+        # Advance block pointers
+        a_block_ptr = tl.advance(a_block_ptr, (0, BLOCK_SIZE_K))
+        b_block_ptr = tl.advance(b_block_ptr, (0, BLOCK_SIZE_K))
+
+    # Apply the square root and store the result
+    c = tl.sqrt(accumulator).to(tl.float16)
+
+    c_block_ptr = tl.make_block_ptr(
+        base=c_ptr,
+        shape=(M, N),
+        strides=(stride_cm, stride_cn),
+        offsets=(pid_m * BLOCK_SIZE_M, pid_n * BLOCK_SIZE_N),
+        block_shape=(BLOCK_SIZE_M, BLOCK_SIZE_N),
+        order=(1, 0),
+    )
+    tl.store(c_block_ptr, c, boundary_check=(0, 1))
+
+
+class _EuclideanDistance(Function):
+
+    @staticmethod
+    def forward(ctx, a: Tensor, b: Tensor) -> Tensor:
+        assert a.shape[-1] == b.shape[-1], "Incompatible dimensions"
+        assert a.is_contiguous(), "Matrix A must be contiguous"
+        assert b.is_contiguous(), "Matrix B must be contiguous"
+
+        M, K = a.shape
+        N, K = b.shape
+        c = torch.empty((M, N), device=a.device, dtype=a.dtype)
+
+        def grid(META):
+            return (triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(N, META["BLOCK_SIZE_N"]),)
+
+        cast(Any, _euclidean_distance_kernel_fwd)[grid](
+            a,
+            b,
+            c,
+            M,
+            N,
+            K,
+            a.stride(0),
+            a.stride(1),
+            b.stride(0),
+            b.stride(1),
+            c.stride(0),
+            c.stride(1),
+        )
+        ctx.save_for_backward(a, b, c)
+
+        return c
+
+
+euclidean_distance: Callable[[Tensor, Tensor], Tensor] = cast(Any, _EuclideanDistance.apply)
 
 
 def _benchmark(
@@ -205,7 +305,7 @@ def parse_args() -> Namespace:
         formatter_class=ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("-MN", type=int, default=8192, help="Max M and N dimension")
-    parser.add_argument("-K", type=int, default=2, help="Fixed size of K dimension")
+    parser.add_argument("-K", type=int, default=[2], nargs="+", help="Fixed size of K dimension")
     parser.add_argument("-s", "--step", type=int, default=128, help="Step size for M and N dimensions")
     parser.add_argument("-o", "--output", type=Path, default=None, help="Output path to save benchmark results")
     parser.add_argument("-w", "--warmup", type=int, default=25, help="`warmup` arg for `triton.testing.do_bench`")
@@ -216,27 +316,34 @@ def parse_args() -> Namespace:
 
 def main(args: Namespace):
     test_configs = list(range(args.step, args.MN + 1, args.step))
-    total_tests = 2 * len(test_configs)
+    total_tests = 2 * len(test_configs) * len(args.K)
     bar = tqdm(total=total_tests, desc="Benchmarking")
 
-    benchmark = triton.testing.perf_report(
-        triton.testing.Benchmark(
-            x_names=["M", "N"],
-            x_vals=test_configs,
-            line_arg="provider",
-            line_vals=["cublas", "triton"],
-            line_names=["cuBLAS", "Triton"],
-            styles=[("green", "-"), ("blue", "-")],
-            ylabel="TFLOPS",
-            plot_name="euclidean-kernel",
-            args={},
+    for k in args.K:
+        benchmark = triton.testing.perf_report(
+            triton.testing.Benchmark(
+                x_names=["M", "N"],
+                x_vals=test_configs,
+                line_arg="provider",
+                line_vals=["cublas", "triton"],
+                line_names=["cuBLAS", "Triton"],
+                styles=[("green", "-"), ("blue", "-")],
+                xlabel=f"Input size (Mx{k}, Mx{k})",
+                ylabel="TFLOPS",
+                plot_name=f"euclidean-kernel-k={k}",
+                args={},
+            )
+        )(partial(_benchmark, warmup=args.warmup, rep=args.rep, bar=bar, verbose=args.verbose))
+        df: Any = benchmark.run(
+            show_plots=False,
+            print_data=False,
+            return_df=True,
+            save_path=args.output,
+            K=k,
         )
-    )(partial(_benchmark, K=args.K, warmup=args.warmup, rep=args.rep))
-    df: Any = benchmark.run(
-        show_plots=False, print_data=False, return_df=True, save_path=args.output, bar=bar, verbose=args.verbose
-    )
+        with tqdm.external_write_mode():
+            print(df.to_string(index=False))
     bar.close()
-    print(df.to_string(index=False))
 
 
 if __name__ == "__main__":
