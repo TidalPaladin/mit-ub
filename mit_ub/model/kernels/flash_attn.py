d6ef04a921b3ef1679104517de233af06cdcf928
Scott Chase Waggener <tidalpaladin@protonmail.com>
2024-02-29 11:36:46 -0600

    WIP

--- mit_ub/model/kernels/flash_attn.py
+++ mit_ub/model/kernels/flash_attn.py
@@ -1,49 +1,38 @@
 import math
 from typing import Final, Tuple
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
+from functools import partial
+from pathlib import Path
+from typing import Any, cast
+from tqdm import tqdm
 
 import torch
+import torch.nn.functional as F
 import triton
 import triton.language as tl
+from .helpers import TENSOR_CORE_K, BoundaryCheckHeuristic, PowerOfTwoHeuristic
 
 
-# In the case of K=16 we will perform the following operation in each tensor core
-# (16x16) * (16x8) = (16x8)
-# BFloat16 will only support a FP32 accumulator
-TENSOR_CORE_MAX_K: Final = 16
-
-
-def _boundary_check(l, d, block_l, block_d, flip) -> Tuple[int, ...] | None:
-    result = []
-    if l % block_l != 0:
-        result.append(0)
-    if d % block_d != 0:
-        result.append(1)
-    if flip:
-        result = result[::-1]
-    return tuple(result) if result else None
-
-
-# Disabling autotune for now, set num_warps=4 if headdim=64 and num_warps=8 if headdim=128
-# @triton.autotune(
-#     configs=[
-#         triton.Config({"BLOCK_M": 128, "BLOCK_N": 128}, num_warps=4, num_stages=1),
-#         # This config has a race condition when EVEN_M == False, disabling it for now.
-#         # triton.Config({"BLOCK_M": 64, "BLOCK_N": 64}, num_warps=4, num_stages=1),
-#     ],
-#     key=['CACHE_KEY_SEQLEN_Q', 'CACHE_KEY_SEQLEN_K', 'BIAS_TYPE', 'IS_CAUSAL', 'BLOCK_HEADDIM']
-# )
-# Boundary checks take time. We will avoid them when possible by using heuristics to determine if they are necessary.
+@triton.autotune(
+    configs=[
+        triton.Config({"BLOCK_M": 16, "BLOCK_N": 16}),
+        triton.Config({"BLOCK_M": 32, "BLOCK_N": 32}),
+        triton.Config({"BLOCK_M": 64, "BLOCK_N": 64}),
+        triton.Config({"BLOCK_M": 128, "BLOCK_N": 128}),
+    ],
+    key=["D", "M", "N"],
+)
 @triton.heuristics(
     {
-        "BOUNDARY_CHECK_Q": lambda args: _boundary_check(
-            args["M"], args["D"], args["BLOCK_M"], args["BLOCK_HEADDIM"], False
-        ),
-        "BOUNDARY_CHECK_K": lambda args: _boundary_check(
-            args["N"], args["D"], args["BLOCK_N"], args["BLOCK_HEADDIM"], True
-        ),
-        "BOUNDARY_CHECK_V": lambda args: _boundary_check(
-            args["N"], args["D"], args["BLOCK_N"], args["BLOCK_HEADDIM"], False
-        ),
+        # BLOCK_HEADDIM must go up to the size of D.
+        "BLOCK_HEADDIM": PowerOfTwoHeuristic("D", min_val=TENSOR_CORE_K),
+        #"BLOCK_M": PowerOfTwoHeuristic("M", min_val=TENSOR_CORE_K, max_val=32),
+        #"BLOCK_N": PowerOfTwoHeuristic("N", min_val=TENSOR_CORE_K, max_val=32),
+        "EVEN_M": BoundaryCheckHeuristic("M", "BLOCK_M", disable=False),
+        "EVEN_N": BoundaryCheckHeuristic("N", "BLOCK_N", disable=False),
+        "EVEN_D": BoundaryCheckHeuristic("D", "BLOCK_HEADDIM", disable=False),
+        "num_warps": lambda args: 4 if args["D"] <= 64 else 8,
+        #"num_warps": lambda args: 4,
     }
 )
 @triton.jit
@@ -93,9 +82,9 @@ def _fwd_kernel(
     BLOCK_N: tl.constexpr,
     BLOCK_HEADDIM: tl.constexpr,
     # Heuristics for boundary checks
-    BOUNDARY_CHECK_Q: tl.constexpr,
-    BOUNDARY_CHECK_K: tl.constexpr,
-    BOUNDARY_CHECK_V: tl.constexpr,
+    EVEN_M: tl.constexpr,
+    EVEN_N: tl.constexpr,
+    EVEN_D: tl.constexpr,
 ):
     # Grid is (L, H * B)
     # Q of shape (B, Lq, H, D)
@@ -111,6 +100,13 @@ def _fwd_kernel(
     offset_h = offset_bh % H
     BHN = B * H * N
     BHM = B * H * M
+
+    #if EVEN_M: 
+    #    tl.multiple_of(M, BLOCK_M)
+    #if EVEN_N:
+    #    tl.multiple_of(N, BLOCK_N)
+    #if EVEN_D:
+    #    tl.multiple_of(D, BLOCK_HEADDIM)
 
     # Initialize pointers
     # This program's block of queries will be loaded by this program for processing and kept there.
@@ -160,18 +156,18 @@ def _fwd_kernel(
     #   * `p` - exp(x_i - c) for each query
     #   * `softmax_denominator` - sum_j exp(x_j - c) for each query
     #   * `alpha` - exp(c - d) for each query
-    query_i_maxdot = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")
+    query_i_maxdot = tl.full([BLOCK_M], float("-inf"), dtype=tl.float32)
     softmax_denominator = tl.zeros([BLOCK_M], dtype=tl.float32)
     value_accumulator = tl.zeros([BLOCK_M, BLOCK_HEADDIM], dtype=tl.float32)
 
     # Q gets loaded into SRAM and stays there. Pre-apply the softmax scale
-    q = tl.load(Q_ptrs, boundary_check=BOUNDARY_CHECK_Q)
+    q = tl.load(Q_ptrs, boundary_check=(1, 0))
     q = (q * softmax_scale).to(k_p.dtype.element_ty)
 
     for _ in range(0, N, BLOCK_N):
         # Load K and V blocks into SRAM
-        k = tl.load(K_block_ptr, boundary_check=BOUNDARY_CHECK_K)
-        v = tl.load(V_block_ptr, boundary_check=BOUNDARY_CHECK_V)
+        k = tl.load(K_block_ptr, boundary_check=(1, 0))
+        v = tl.load(V_block_ptr, boundary_check=(1, 0))
 
         # Compute QK
         qk = tl.dot(q, k, allow_tf32=True)
@@ -215,7 +211,7 @@ def _fwd_kernel(
         block_shape=(BLOCK_M, BLOCK_HEADDIM),
         order=(1, 0),
     )
-    tl.store(O_block_ptr, value_accumulator.to(out_p.dtype.element_ty), boundary_check=BOUNDARY_CHECK_Q)
+    tl.store(O_block_ptr, value_accumulator.to(out_p.dtype.element_ty), boundary_check=(1, 0))
 
 
 def _flash_attn_forward(q, k, v, bias=None, causal=False, softmax_scale=None):
@@ -247,16 +243,13 @@ def _flash_attn_forward(q, k, v, bias=None, causal=False, softmax_scale=None):
     (bias.stride(0), bias.stride(1), bias.stride(2)) if has_bias else (0, 0, 0)
 
     seqlen_q_rounded = math.ceil(seqlen_q / 128) * 128
-    lse = torch.empty((batch, nheads, seqlen_q_rounded), device=q.device, dtype=torch.float32)
-    tmp = torch.empty((batch, nheads, seqlen_q_rounded), device=q.device, dtype=torch.float32)
+    #lse = torch.empty((batch, nheads, seqlen_q_rounded), device=q.device, dtype=torch.float32)
+    lse = None
+    #tmp = torch.empty((batch, nheads, seqlen_q_rounded), device=q.device, dtype=torch.float32)
     o = torch.empty_like(q)
     B = q.shape[0]
     H = q.shape[2]
     D = q.shape[3]
-
-    BLOCK_HEADDIM = max(triton.next_power_of_2(d), TENSOR_CORE_MAX_K)
-    BLOCK = 16
-    num_warps = 4 if d <= 64 else 8
 
     def grid(META):
         return (triton.cdiv(seqlen_q, META["BLOCK_M"]), batch * nheads)
@@ -284,10 +277,123 @@ def _flash_attn_forward(q, k, v, bias=None, causal=False, softmax_scale=None):
         o.stride(0),
         o.stride(1),
         o.stride(2),
-        BLOCK_M=BLOCK,
-        BLOCK_N=BLOCK,
-        BLOCK_HEADDIM=BLOCK_HEADDIM,
-        # num_warps=num_warps,
-        # num_stages=1,
     )
     return o, lse, softmax_scale  # softmax_scale could have been updated
+
+
+def _benchmark(
+    Q: int,
+    K: int,
+    D: int,
+    provider: str,
+    warmup: int = 25,
+    rep: int = 100,
+    bar: tqdm | None = None,
+    verbose: bool = False,
+):
+    if verbose:
+        with tqdm.external_write_mode():
+            print(f"Running benchmark for Lq={Q}, Lk={K}, D={D}, provider={provider}")
+    B = 4
+    N_head = 8
+
+    q = torch.randn((B, Q, N_head, D), device="cuda", dtype=torch.float16)
+    k = torch.randn((B, K, N_head, D), device="cuda", dtype=torch.float16)
+    v = torch.randn((B, K, N_head, D), device="cuda", dtype=torch.float16)
+    quantiles = [0.5, 0.2, 0.8]
+
+    match provider:
+        case "torch" | "flash" | "mem-eff":
+            q = q.movedim(1, 2)
+            k = k.movedim(1, 2)
+            v = v.movedim(1, 2)
+            with torch.backends.cuda.sdp_kernel(
+                enable_flash=provider == "flash", 
+                enable_math=provider == "torch",
+                enable_mem_efficient=provider == "mem-eff",
+            ):
+                ms, min_ms, max_ms = triton.testing.do_bench(
+                    lambda: F.scaled_dot_product_attention(q, k, v),
+                    quantiles=quantiles,
+                    warmup=warmup,
+                    rep=rep,
+                )
+        case "triton":
+            ms, min_ms, max_ms = triton.testing.do_bench(
+                lambda: _flash_attn_forward(q, k, v, causal=False, softmax_scale=D ** -0.5),
+                quantiles=quantiles,
+                warmup=warmup,
+                rep=rep,
+            )
+        case _:
+            raise ValueError(f"Unknown provider: {provider}")
+
+    def perf(_ms):
+        return _ms
+
+    if bar is not None:
+        bar.update(1)
+
+    return perf(ms), perf(max_ms), perf(min_ms)
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser(
+        description="",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-QK", type=int, default=8192, help="Max Q and K length")
+    parser.add_argument("-D", type=int, default=[32], nargs="+", help="Fixed size of D dimension")
+    parser.add_argument("-s", "--step", type=int, default=128, help="Step size for Q and K dimensions")
+    parser.add_argument("-o", "--output", type=Path, default=None, help="Output path to save benchmark results")
+    parser.add_argument("--warmup", type=int, default=25, help="`warmup` arg for `triton.testing.do_bench`")
+    parser.add_argument("--rep", type=int, default=100, help="`rep` arg for `triton.testing.do_bench`")
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Print each benchmark result")
+    return parser.parse_args()
+
+
+def main(args: Namespace):
+    test_configs = list(range(args.step, args.QK + 1, args.step))
+    providers = ["torch", "flash", "mem-eff", "triton"]
+    total_tests = len(providers) * len(test_configs) * len(args.D)
+    bar = tqdm(total=total_tests, desc="Benchmarking")
+
+    for d in args.D:
+        plot_name = f"flash-attn-d={d}"
+        xlabel = f"Input size (Lx{d}, Lx{d})" 
+        benchmark = triton.testing.perf_report(
+            triton.testing.Benchmark(
+                x_names=["Q", "K"],
+                x_vals=test_configs,
+                line_arg="provider",
+                line_vals=providers,
+                line_names=["Torch", "Flash Attn", "ME Attn", "Triton"],
+                styles=[("green", "-"), ("orange", "-"), ("blue", "-"), ("red", "-")],
+                xlabel=xlabel,
+                ylabel="Latency (ms)",
+                plot_name=plot_name,
+                args={},
+            )
+        )(
+            partial(
+                _benchmark,
+                warmup=args.warmup,
+                rep=args.rep,
+                bar=bar,
+                verbose=args.verbose,
+            )
+        )
+        df: Any = benchmark.run(
+            show_plots=False,
+            print_data=False,
+            return_df=True,
+            save_path=args.output,
+            D=d,
+        )
+        with tqdm.external_write_mode():
+            print(df.to_string(index=False))
+    bar.close()
+
+
+if __name__ == "__main__":
+    main(parse_args())
--- tests/test_model/test_kernels/test_flash_attn.py
+++ tests/test_model/test_kernels/test_flash_attn.py
@@ -5,20 +5,20 @@ import torch.nn.functional as F
 from mit_ub.model.kernels.flash_attn import _flash_attn_forward
 
 
-def test_flash_attn_forward():
+@pytest.mark.parametrize("b, lq, lk, dhead, nhead", [
+    (1, 32, 32, 16, 16),
+    (1, 4, 4, 4, 1),
+    (3, 30, 33, 24, 3),
+])
+def test_flash_attn_forward(b, lq, lk, dhead, nhead):
     if not torch.cuda.is_available():
         pytest.skip("CUDA is not available")
     torch.manual_seed(0)
 
-    B = 2
-    Lq = 32
-    Lk = 32
-    D_head = 32
-    N_head = 2
-    q = torch.randn((B, Lq, N_head, D_head), device="cuda", dtype=torch.float16)
-    k = torch.randn((B, Lk, N_head, D_head), device="cuda", dtype=torch.float16)
-    v = torch.randn((B, Lk, N_head, D_head), device="cuda", dtype=torch.float16)
+    q = torch.randn((b, lq, nhead, dhead), device="cuda", dtype=torch.float16)
+    k = torch.randn((b, lk, nhead, dhead), device="cuda", dtype=torch.float16)
+    v = torch.randn((b, lk, nhead, dhead), device="cuda", dtype=torch.float16)
 
     baseline_output = F.scaled_dot_product_attention(q.movedim(1, 2), k.movedim(1, 2), v.movedim(1, 2)).movedim(2, 1)
-    triton_output, _, _ = _flash_attn_forward(q, k, v, causal=False, softmax_scale=D_head**-0.5)
+    triton_output, _, _ = _flash_attn_forward(q, k, v, causal=False, softmax_scale=dhead**-0.5)
     assert torch.allclose(baseline_output, triton_output, atol=0.01, rtol=0)
