63f8716b8bad876e0dd97e6590d878c818f03341
Scott Chase Waggener <tidalpaladin@protonmail.com>
2024-06-19 09:30:37 -0500

    Improve ALiBi initialization

--- tests/test_model/test_backbone.py
+++ tests/test_model/test_backbone.py
@@ -1,10 +1,28 @@
 import pytest
 import torch
+from torch.testing import assert_close
 
 from mit_ub.model.backbone import TransformerBlock, ViT
 
 
 class TestTransformerBlock:
+
+    @pytest.mark.parametrize(
+        "lower,upper,nhead,exp",
+        [
+            pytest.param(None, 8, 8, None, marks=pytest.mark.xfail(raises=ValueError)),
+            pytest.param(8, None, 8, None, marks=pytest.mark.xfail(raises=ValueError)),
+            (None, None, 8, None),
+            (0, 8, 8, torch.tensor([-1.0000, -0.4529, -0.2051, -0.0929, -0.0421, -0.0190, -0.0086, -0.0039])),
+            (0, 8, 4, torch.tensor([-1.0000, -0.1575, -0.0248, -0.0039])),
+            (-2, 0, 3, torch.tensor([-4.0000, -2.0000, -1.0000])),
+        ],
+    )
+    def test_alibi_slopes(self, lower, upper, nhead, exp):
+        D = nhead
+        layer = TransformerBlock(D, nhead, D, alibi_lower=lower, alibi_upper=upper)
+        slopes = layer.alibi
+        assert_close(slopes, exp, atol=1e-4, rtol=0)
 
     @pytest.mark.parametrize(
         "device",
@@ -42,6 +60,7 @@ class TestTransformerBlock:
 
 class TestViT:
 
+    @pytest.mark.parametrize("alibi", [False, True])
     @pytest.mark.parametrize(
         "device",
         [
@@ -49,10 +68,10 @@ class TestViT:
             pytest.param("cuda", marks=pytest.mark.cuda),
         ],
     )
-    def test_forward(self, device):
+    def test_forward(self, device, alibi):
         x = torch.randn(1, 3, 224, 224, device=device)
         nhead = 128 // 16
-        model = ViT(3, 128, (16, 16), 3, nhead).to(device)
+        model = ViT(3, 128, (16, 16), 3, nhead, alibi=alibi).to(device)
         with torch.autocast(device_type=device, dtype=torch.float16):
             out = model(x)
         assert out.shape[:2] == (1, 128)
