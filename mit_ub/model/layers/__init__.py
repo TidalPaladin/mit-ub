e91383b44d15f8ae1e178be82d82122eacd46cb8
Scott Chase Waggener <chase.waggener@icloud.com>
2024-12-26 15:17:52 +0000

    Improve SoftMoE stability

--- mit_ub/model/layers/__init__.py
+++ mit_ub/model/layers/__init__.py
@@ -4,7 +4,7 @@ from .layer_scale import LayerScale, has_layer_scale
 from .mlp import MLP, NormType, mlp_forward
 from .pool import AveragePool, MaxPool, MultiHeadAttentionPool
 from .pos_enc import RelativeFactorizedPosition, relative_factorized_position_forward
-from .soft_moe import SoftMoE, soft_moe_forward
+from .soft_moe import SoftMoE
 from .transformer import (
     TransformerConvDecoderLayer,
     TransformerConvEncoderLayer,
@@ -25,7 +25,6 @@ __all__ = [
     "RelativeFactorizedPosition",
     "relative_factorized_position_forward",
     "SoftMoE",
-    "soft_moe_forward",
     "TransformerDecoderLayer",
     "TransformerEncoderLayer",
     "TransformerConvEncoderLayer",
