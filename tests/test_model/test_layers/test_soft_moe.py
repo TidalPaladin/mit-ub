dccf9958b3f9d89cf87e35ef128bc8c4d56cbe38
Scott Chase Waggener <tidalpaladin@protonmail.com>
2024-12-26 10:00:42 -0600

    Improve SoftMoE stability (#88)
    * Improve SoftMoE stability
    
    * Add norm property

--- tests/test_model/test_layers/test_soft_moe.py
+++ tests/test_model/test_layers/test_soft_moe.py
@@ -1,43 +1,12 @@
 import pytest
 import torch
 import torch.nn.functional as F
-from torch.testing import assert_close
 
-from mit_ub.model.layers.mlp import NormType, mlp_forward
-from mit_ub.model.layers.soft_moe import SoftMoE, forward_experts
+from mit_ub.model.layers.mlp import NormType
+from mit_ub.model.layers.soft_moe import SoftMoE
 
 
 class TestSoftMoE:
-
-    @pytest.mark.parametrize("dropout", [0.0, 0.1])
-    @pytest.mark.parametrize("output_dropout", [False, True])
-    def test_forward_single_expert(self, dropout, output_dropout):
-        B, S, D, E = 2, 128, 128, 1
-        w_in = torch.randn(E, 2 * D, D)
-        b_in = torch.randn(E, 2 * D)
-        w_out = torch.randn(E, D, 2 * D)
-        b_out = torch.randn(E, D)
-        w_gate = torch.randn(E, 2 * D, D)
-        b_gate = torch.randn(E, 2 * D)
-        x = torch.randn(B, S, D)
-
-        torch.manual_seed(0)
-        out1 = forward_experts(x, w_in, b_in, w_out, b_out, F.relu, dropout, w_gate, b_gate, F.relu, training=True)
-        torch.manual_seed(0)
-        out2 = mlp_forward(
-            x,
-            w_in.squeeze(0),
-            w_out.squeeze(0),
-            b_in.squeeze(0),
-            b_out.squeeze(0),
-            w_gate.squeeze(0),
-            b_gate.squeeze(0),
-            dropout,
-            F.relu,
-            F.relu,
-            training=True,
-        )
-        assert_close(out1, out2, atol=0.01, rtol=0)
 
     @pytest.mark.parametrize(
         "device",
@@ -68,20 +37,23 @@ class TestSoftMoE:
             pytest.param("cuda", marks=pytest.mark.cuda),
         ],
     )
-    def test_backward(self, device):
+    @pytest.mark.parametrize("norm", [False, True])
+    def test_backward(self, device, norm):
         B, L, D = 1, 128, 128
         nhead = D // 32
         num_experts = 4
         num_slots = L
 
         x = torch.randn(B, L, D, device=device, requires_grad=True)
-        layer = SoftMoE(D, D, num_experts, num_slots, nhead=nhead).to(device)
+        layer = SoftMoE(D, D, num_experts, num_slots, nhead=nhead, norm=norm).to(device)
 
         with torch.autocast(device_type=device, dtype=torch.float16):
             out = layer(x)
         assert out.shape == x.shape
         out.sum().backward()
         assert x.grad is not None
+        for p in layer.parameters():
+            assert p.grad is not None
 
     @pytest.mark.parametrize(
         "device",
@@ -131,26 +103,7 @@ class TestSoftMoE:
                 continue
             assert not torch.allclose(v, weight_reset[k], equal_nan=True)
 
-    def test_fused_norm(self):
-        torch.random.manual_seed(0)
-        B, L, D = 1, 128, 128
-        nhead = D // 32
-        num_experts = 4
-        num_slots = 16
-
-        x = torch.randn(B, L, D)
-        layer = SoftMoE(D, D, num_experts, num_slots, nhead=nhead, norm=True)
-
-        y_norm = layer(x)
-        layer.w_pre_norm = None  # type: ignore
-        layer.b_pre_norm = None  # type: ignore
-        y_no_norm = layer(x)
-        assert not torch.allclose(y_norm, y_no_norm)
-
     def test_extra_repr(self):
         layer = SoftMoE(32, 64, 4, 16, nhead=8)
         result = str(layer)
-        assert (
-            result
-            == "SoftMoE(in=32, hidden=64, out=32, experts=4, slots=16, nhead=8, dropout=0.0, act=relu2, gate_act=None, bias=True, norm=False, qk_norm=False, norm_type=layernorm)"
-        )
+        assert "num_experts=4, num_slots=16" in result
