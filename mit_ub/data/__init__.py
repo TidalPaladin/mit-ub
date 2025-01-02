846e29e2711496a2f580654a51e6994adfbc14e2
Scott Chase Waggener <tidalpaladin@protonmail.com>
2025-01-02 12:33:04 -0600

    Speed up batched noise application (#111)
    * Speed up batched noise application
    
    * Fixes and tweaks

--- mit_ub/data/__init__.py
+++ mit_ub/data/__init__.py
@@ -1,7 +1,7 @@
 from .cifar10 import CIFAR10DataModule
 from .cifar100 import CIFAR100DataModule
 from .mixup import is_mixed, is_mixed_with_unknown, mixup, mixup_dense_label, sample_mixup_parameters
-from .noise import RandomNoise
+from .noise import RandomNoise, apply_noise_batched
 from .rotation import RandomRotation
 
 
@@ -15,4 +15,5 @@ __all__ = [
     "is_mixed",
     "RandomNoise",
     "is_mixed_with_unknown",
+    "apply_noise_batched",
 ]
--- mit_ub/data/noise.py
+++ mit_ub/data/noise.py
@@ -1,12 +1,30 @@
-from typing import Any, Dict, List, Sequence, Tuple
+from typing import Any, Dict, Final, List, Sequence, Tuple, cast
 
 import torch
 from torch import Tensor
-from torchvision.transforms.v2 import Compose, RandomApply, RandomChoice, Transform
+from torchvision.transforms.v2 import Compose, RandomApply, Transform
 
 
-# @torch.compile(fullgraph=True, disable=compile_is_disabled())
-def uniform_noise(x: Tensor, min: float = -0.2, max: float = 0.2, clip: bool = True) -> Tensor:
+UNIFORM_NOISE_MIN: Final = -0.2
+UNIFORM_NOISE_MAX: Final = 0.2
+MULTIPLICATIVE_NOISE_MIN: Final = 0.02
+MULTIPLICATIVE_NOISE_MAX: Final = 0.2
+SALT_PEPPER_NOISE_MIN: Final = 0.01
+SALT_PEPPER_NOISE_MAX: Final = 0.05
+
+
+def to_tuple(x: float | Sequence[float]) -> Tuple[float, float]:
+    if isinstance(x, Sequence):
+        assert len(x) == 2, "Expected a sequence of length 2"
+        return cast(Tuple[float, float], tuple(x))
+    else:
+        return (x, x)
+
+
+@torch.no_grad()
+def uniform_noise(
+    x: Tensor, min: float = UNIFORM_NOISE_MIN, max: float = UNIFORM_NOISE_MAX, clip: bool = True
+) -> Tensor:
     """Apply uniform noise to tensor with same shape as input.
 
     Args:
@@ -19,55 +37,56 @@ def uniform_noise(x: Tensor, min: float = -0.2, max: float = 0.2, clip: bool = T
         Input with uniform noise applied
     """
     noise = torch.empty_like(x).uniform_(min, max)
-    result = x + noise
+    result = noise.add_(x)
     if clip:
         result.clip_(min=0, max=1)
     return result
 
 
-# @torch.compile(fullgraph=True, disable=compile_is_disabled())
-def salt_pepper_noise(x: Tensor, prob: float | Tuple[float, float] = (0.01, 0.05)) -> Tensor:
+@torch.no_grad()
+def salt_pepper_noise(x: Tensor, min: float = SALT_PEPPER_NOISE_MIN, max: float = SALT_PEPPER_NOISE_MAX) -> Tensor:
     """Apply salt and pepper noise to tensor with same shape as input.
 
     Args:
         x: Input tensor to match shape
-        prob: Probability of setting a pixel to min or max, or a tuple of the form (min, max)
-            giving the range of the uniform distribution to sample from
+        min: Minimum probability of setting a pixel to min or max
+        max: Maximum probability of setting a pixel to min or max
 
     Returns:
         Input with salt and pepper noise applied
     """
-    if isinstance(prob, Sequence):
-        low, high = prob
-        p = x.new_empty(1).uniform_(low, high)
-    else:
-        p = x.new_tensor(prob)
-    mask = torch.rand_like(x) < p
-    value = torch.rand_like(x).round_()
+    p = x.new_empty(1).uniform_(min, max)
+    mask = torch.rand_like(x).lt_(p).bool()
+    value = torch.randint_like(x, 0, 2)
     return torch.where(mask, value, x)
 
 
-# @torch.compile(fullgraph=True, disable=compile_is_disabled())
-def multiplicative_noise(x: Tensor, scale: float = 0.2, clip: bool = True) -> Tensor:
+@torch.no_grad()
+def multiplicative_noise(
+    x: Tensor, min: float = MULTIPLICATIVE_NOISE_MIN, max: float = MULTIPLICATIVE_NOISE_MAX, clip: bool = True
+) -> Tensor:
     """Apply multiplicative noise to tensor with same shape as input.
 
     Args:
         x: Input tensor to match shape
-        scale: Scale factor for noise
+        min: Minimum scale factor for noise
+        max: Maximum scale factor for noise
         clip: Whether to clip the result to the range :math:`[0, 1]`
 
     Returns:
         Input with multiplicative noise applied
     """
-    result = x * (1 + torch.randn_like(x) * scale)
+    scale = torch.empty(1).uniform_(min, max).item()
+    result = torch.empty_like(x).normal_(mean=1.0, std=scale).clip_(min=0, max=2)
+    result = result.mul_(x)
     if clip:
-        result = torch.clamp(result, min=0, max=1)
+        result.clamp_(min=0, max=1)
     return result
 
 
 class UniformNoise(Transform):
 
-    def __init__(self, min: float = -0.2, max: float = 0.2, clip: bool = True):
+    def __init__(self, min: float = UNIFORM_NOISE_MIN, max: float = UNIFORM_NOISE_MAX, clip: bool = True):
         super().__init__()
         self.min = min
         self.max = max
@@ -87,9 +106,9 @@ class UniformNoise(Transform):
 
 class SaltPepperNoise(Transform):
 
-    def __init__(self, prob: float | Tuple[float, float] = (0.01, 0.05)):
+    def __init__(self, prob: float | Tuple[float, float] = (SALT_PEPPER_NOISE_MIN, SALT_PEPPER_NOISE_MAX)):
         super().__init__()
-        self.prob = prob
+        self.min, self.max = to_tuple(prob)
 
     def _get_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
         return dict()
@@ -97,15 +116,17 @@ class SaltPepperNoise(Transform):
     def _transform(self, inpt: Any, params: Dict[str, Any]) -> Any:
         return salt_pepper_noise(
             inpt,
-            prob=self.prob,
+            min=self.min,
+            max=self.max,
         )
 
 
 class MultiplicativeNoise(Transform):
 
-    def __init__(self, scale: float = 0.2, clip: bool = True):
+    def __init__(self, min: float = MULTIPLICATIVE_NOISE_MIN, max: float = MULTIPLICATIVE_NOISE_MAX, clip: bool = True):
         super().__init__()
-        self.scale = scale
+        self.min = min
+        self.max = max
         self.clip = clip
 
     def _get_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
@@ -114,7 +135,8 @@ class MultiplicativeNoise(Transform):
     def _transform(self, inpt: Any, params: Dict[str, Any]) -> Any:
         return multiplicative_noise(
             inpt,
-            scale=self.scale,
+            min=self.min,
+            max=self.max,
             clip=self.clip,
         )
 
@@ -123,20 +145,83 @@ class RandomNoise(Compose):
 
     def __init__(
         self,
-        scale: float = 0.2,
-        salt_pepper_prob: float | Tuple[float, float] = (0.01, 0.05),
+        prob: float = 0.5,
+        uniform_scale: float | Tuple[float, float] = (UNIFORM_NOISE_MIN, UNIFORM_NOISE_MAX),
+        multiplicative_scale: float | Tuple[float, float] = (MULTIPLICATIVE_NOISE_MIN, MULTIPLICATIVE_NOISE_MAX),
+        salt_pepper_prob: float | Tuple[float, float] = (SALT_PEPPER_NOISE_MIN, SALT_PEPPER_NOISE_MAX),
         clip: bool = True,
     ):
-        primary_noise = RandomApply(
-            [
-                RandomChoice(
-                    [
-                        UniformNoise(min=-scale, max=scale, clip=clip),
-                        MultiplicativeNoise(scale=scale, clip=clip),
-                    ]
-                )
-            ],
-            p=0.5,
+        self.uniform_scale = to_tuple(uniform_scale)
+        self.multiplicative_scale = to_tuple(multiplicative_scale)
+        self.salt_pepper_prob = to_tuple(salt_pepper_prob)
+        self.clip = clip
+        uniform_noise = RandomApply(
+            [UniformNoise(min=self.uniform_scale[0], max=self.uniform_scale[1], clip=self.clip)], p=prob
         )
-        salt_pepper_noise = RandomApply([SaltPepperNoise(prob=salt_pepper_prob)], p=0.5)
-        super().__init__([primary_noise, salt_pepper_noise])
+        multiplicative_noise = RandomApply(
+            [MultiplicativeNoise(min=self.multiplicative_scale[0], max=self.multiplicative_scale[1], clip=self.clip)],
+            p=prob,
+        )
+        salt_pepper_noise = RandomApply([SaltPepperNoise(prob=self.salt_pepper_prob)], p=prob)
+        super().__init__([uniform_noise, multiplicative_noise, salt_pepper_noise])
+
+    def apply_batched(self, x: Tensor) -> Tensor:
+        return apply_noise_batched(
+            x,
+            prob=0.5,
+            uniform_scale=self.uniform_scale,
+            multiplicative_scale=self.multiplicative_scale,
+            salt_pepper_prob=self.salt_pepper_prob,
+            clip=self.clip,
+        )
+
+
+@torch.no_grad()
+def apply_noise_batched(
+    x: Tensor,
+    prob: float = 0.5,
+    uniform_scale: float | Tuple[float, float] = (UNIFORM_NOISE_MIN, UNIFORM_NOISE_MAX),
+    multiplicative_scale: float | Tuple[float, float] = (MULTIPLICATIVE_NOISE_MIN, MULTIPLICATIVE_NOISE_MAX),
+    salt_pepper_prob: float | Tuple[float, float] = (SALT_PEPPER_NOISE_MIN, SALT_PEPPER_NOISE_MAX),
+    clip: bool = True,
+) -> Tensor:
+    r"""Applies noise to a batch of images such that each image in the batch is
+    independently transformed. This applies all of the noise types in sequence.
+
+    Args:
+        x: Input tensor
+        prob: Probability of applying noise to the input
+        uniform_scale: Scale of the uniform noise to apply to the input
+        multiplicative_scale: Scale of the multiplicative noise to apply to the input
+        salt_pepper_prob: Proportion of salt and pepper noise to apply to the input
+        clip: Whether to clip the result to the range :math:`[0, 1]`
+
+    Shape:
+        - Input: :math:`(N, ...)`
+        - Output: Same shape as input
+
+    Returns:
+        Input with noise applied
+    """
+    N = x.shape[0]
+    x = x.clone()
+
+    # Uniform noise
+    mask = torch.rand(N, device=x.device).lt_(prob).bool()
+    min, max = to_tuple(uniform_scale)
+    uniform_noised_x = uniform_noise(x, min=min, max=max, clip=clip)
+    x[mask] = uniform_noised_x[mask]
+
+    # Multiplicative noise
+    mask = torch.rand(N, device=x.device).lt_(prob).bool()
+    min, max = to_tuple(multiplicative_scale)
+    multiplicative_noised_x = multiplicative_noise(x, min=min, max=max, clip=clip)
+    x[mask] = multiplicative_noised_x[mask]
+
+    # Salt and pepper noise
+    mask = torch.rand(N, device=x.device).lt_(prob).bool()
+    min, max = to_tuple(salt_pepper_prob)
+    salt_pepper_noised_x = salt_pepper_noise(x, min=min, max=max)
+    x[mask] = salt_pepper_noised_x[mask]
+
+    return x
